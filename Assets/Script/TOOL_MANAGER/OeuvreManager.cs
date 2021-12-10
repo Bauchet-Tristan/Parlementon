f903f8080b7f5f3d28236aaa95ab3397b2c9ab4d
Bauchet-Tristan <t.bauchet@rns.ecolescreatives.com>
2021-12-10 14:07:42 +0100

    up tuto

--- Assets/Script/TOOL_MANAGER/OeuvreManager.cs
+++ Assets/Script/TOOL_MANAGER/OeuvreManager.cs
@@ -13,11 +13,11 @@ public class OeuvreManager : MonoBehaviour
     public void AddOeuvre()
     {
         oeuvreCompteur = oeuvreCompteur + 1;
-        affichageCompteur.text = "Oeuvres sauvez : " + oeuvreCompteur.ToString("00");
+        affichageCompteur.text = "Oeuvres sauvées : " + oeuvreCompteur.ToString("00");
 
         if(oeuvreCompteur >= 3)
         {
-            textVictoire.text = "C'est gagnée vous avez sauvé toutes les oeuvres / Fin de l'Alpha";
+            textVictoire.text = "C'est gagné vous avez sauvé toutes les oeuvres / Fin de l'Alpha";
         }
     }
 
